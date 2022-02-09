a752639727fc3f9e54c71595180852221d635ae8
kimnpop <kkrlcakes@gmail.com>
2022-02-10 01:52:28 +0900

    이메일 로그인 폼 제작

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,24 @@
 import Login from '/components/login/Login';
 import useMe from 'hooks/useMe';
+import styled from '@emotion/styled';
 
 const Layout = ({ children }) => {
   const { data: isLogged } = useMe();
 
-  if (!isLogged) return <Login />;
+  if (!isLogged)
+    return (
+      <Container>
+        <Login />
+      </Container>
+    );
 
-  return <div>{children}</div>;
+  return <Container>{children}</Container>;
 };
 
 export default Layout;
+
+const Container = styled.div`
+  width: 100vw;
+  height: auto;
+  min-height: 100vh;
+`;
--- components/login/Login.js
+++ components/login/Login.js
@@ -1,27 +1,57 @@
 import styled from '@emotion/styled';
 import useMe from 'hooks/useMe';
+import { useState } from 'react';
 import Tutorial from '/components/login/Tutorial';
 
 const Login = () => {
   const { mutate } = useMe();
+  const [isEmailLoginFormShown, setIsEmailLoginFormShown] = useState(false);
 
   const handleKakaoLogin = () => {
     mutate(true, false);
   };
   const handleEmailLogin = () => {
-    mutate(true, false);
+    setIsEmailLoginFormShown(true);
   };
 
   return (
     <Container>
       <Tutorial className='login__tutorial' />
-      <button className='login__login-button kakao' onClick={handleKakaoLogin}>
-        <img className='kakao-icon' src='icons/kakao.svg' alt='kakao-icon' />
-        <span>카카오로 시작하기</span>
-      </button>
-      <button className='login__login-button normal' onClick={handleEmailLogin}>
-        <span>이메일로 시작하기</span>
-      </button>
+
+      {!isEmailLoginFormShown ? (
+        <div>
+          <button
+            className='login__login-button kakao'
+            onClick={handleKakaoLogin}
+          >
+            <img
+              className='kakao-icon'
+              src='icons/kakao.svg'
+              alt='kakao-icon'
+            />
+            <span>카카오로 시작하기</span>
+          </button>
+          <button
+            className='login__login-button normal'
+            onClick={handleEmailLogin}
+          >
+            <span>이메일로 시작하기</span>
+          </button>
+        </div>
+      ) : (
+        <EmailLoginForm>
+          <div className='inputLabel'>
+            <input type='text' placeholder='slowbird@gmail.com' id='email' />
+            <label htmlFor='email'>이메일</label>
+          </div>
+          <div className='inputLabel'>
+            <input type='password' placeholder='6자리 이상' id='password' />
+            <label htmlFor='password'>비밀번호</label>
+          </div>
+          <button className='submit-button'>로그인</button>
+          <div className='sign-up'>회원 가입하기</div>
+        </EmailLoginForm>
+      )}
     </Container>
   );
 };
@@ -31,24 +61,15 @@ export default Login;
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-  padding: 20px;
+  padding: 20px 20px 80px 20px;
   overflow-y: hidden;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  .login {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-  }
-
   .login__tutorial {
     width: 100%;
-    height: 80%;
+    height: 100%;
     background: rgba(0, 0, 0, 0.5);
   }
 
@@ -77,8 +98,8 @@ const Container = styled.div`
   }
 
   .normal {
-    background: var(--grey200);
-    border-color: var(--grey200);
+    background: var(--grey100);
+    border-color: var(--grey100);
     color: black;
   }
 
@@ -89,5 +110,65 @@ const Container = styled.div`
     transform: translateY(-50%);
     width: 32px;
     height: 32px;
+  }
+`;
+
+const EmailLoginForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  .inputLabel {
+    position: relative;
+    font-size: 14px;
+
+    label {
+      position: absolute;
+      top: 8px;
+      left: 12px;
+      color: var(--grey700);
+    }
+
+    input {
+      width: 400px;
+      max-width: 100%;
+      height: 60px;
+      margin-bottom: 12px;
+      border-radius: 16px;
+      padding: 24px 12px 0 12px;
+      font-size: 16px;
+      font-weight: 400;
+      border: none;
+      border: 1px solid var(--grey400);
+      color: var(--grey900);
+      font-weight: 300;
+
+      ::placeholder {
+        color: var(--grey400);
+        font-weight: 300;
+      }
+    }
+  }
+
+  .submit-button {
+    width: 400px;
+    max-width: 100%;
+    height: 60px;
+    margin-bottom: 12px;
+    border-radius: 16px;
+    font-size: 16px;
+    font-weight: 400;
+    background: var(--blue500);
+    color: white;
+  }
+
+  .sign-up {
+    margin-top: 8px;
+    font-size: 14px;
+    color: black;
+    font-weight: 300;
+    text-decoration: underline;
+    text-underline-position: under;
   }
 `;
