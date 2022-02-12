f6d33da8c0dec6bcc7864854ce13c70f9ce2e536
kimnpop <kkrlcakes@gmail.com>
2022-02-12 23:01:56 +0900

    닉네임 설정 화면 디자인

--- components/signup/SNSSignup.js
+++ components/signup/SNSSignup.js
@@ -1,45 +1,42 @@
-import React, { useState } from 'react'
-import { useRouter } from 'next/router'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 
 import apiController from 'helpers/apiController';
 
 const SNSSingup = () => {
-  const router = useRouter()
-  const [nickname, setNickname] = useState('')
+  const router = useRouter();
+  const [nickname, setNickname] = useState('');
 
   const handleRegister = () => {
     if (nickname.length === 0) {
-      alert('닉네임을 입력해주세요!')
-      return
+      alert('닉네임을 입력해주세요!');
+      return;
     }
 
-    apiController().post(
-      '/api/user/set-nickname',
-      { nickname: nickname }
-    ).catch(error => {
-      const { status } = error.response
-      console.log("status code", status)
-      alert('닉네임이 중복됩니다!')
-    }).then(res => {
-      router.push('/')
-    })
-  }
+    apiController()
+      .post('/api/user/set-nickname', { nickname: nickname })
+      .catch((error) => {
+        const { status } = error.response;
+        console.log('status code', status);
+        alert('닉네임이 중복됩니다!');
+      })
+      .then((res) => {
+        router.push('/');
+      });
+  };
 
   const handleLater = () => {
     // TODO 이거 필요할까?
-  }
+  };
 
   const handleChangeNickname = (e) => {
-    setNickname(e.target.value)
-  }
+    setNickname(e.target.value);
+  };
 
   return (
     <Container>
-      SNS Signup
-      <Title>
-        고객님의 닉네임을 설정해주세요.
-      </Title>
+      <Title>닉네임을 설정해주세요.</Title>
       <NicknameForm>
         <div className='inputLabel'>
           <input
@@ -50,23 +47,22 @@ const SNSSingup = () => {
           />
         </div>
       </NicknameForm>
-      <button
-        className='login__login-button normal'
-        onClick={handleRegister}
-      >
-        가입하기
-      </button>
-      <button
-        className='login__login-button normal'
-        onClick={handleLater}
-      >
-        나중에
-      </button>
+      <Buttons>
+        <button
+          className='login__login-button primary'
+          onClick={handleRegister}
+        >
+          가입하기
+        </button>
+        <button className='login__login-button secondary' onClick={handleLater}>
+          나중에
+        </button>
+      </Buttons>
     </Container>
-  )
-}
+  );
+};
 
-export default SNSSingup
+export default SNSSingup;
 
 const Container = styled.div`
   height: 100vh;
@@ -76,6 +72,49 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  text-align: left;
+  font-size: 20px;
+  font-weight: 500;
+  margin: 20px 0;
+`;
+
+const NicknameForm = styled.div`
+  width: 100%;
+  flex: 1;
+
+  .inputLabel {
+    position: relative;
+    width: 100%;
+    font-size: 14px;
+
+    input {
+      width: 400px;
+      max-width: 100%;
+      height: 60px;
+      margin: 8px 0 40px 0;
+      border-radius: 16px;
+      padding: 0 12px;
+      font-size: 16px;
+      font-weight: 400;
+      border: none;
+      border: 1px solid var(--grey400);
+      color: var(--grey900);
+      font-weight: 300;
+
+      ::placeholder {
+        color: var(--grey400);
+        font-weight: 300;
+      }
+    }
+  }
+`;
+
+const Buttons = styled.div`
+  width: 100%;
 
   .login__login-button {
     width: 400px;
@@ -94,46 +133,15 @@ const Container = styled.div`
       opacity: 0.6;
     }
   }
-  .normal {
+  .primary {
+    background: var(--blue500);
+    border-color: var(--blue500);
+    color: white;
+  }
+
+  .secondary {
     background: var(--grey100);
     border-color: var(--grey100);
     color: black;
   }
-`
-
-const Title = styled.h2``
-
-const NicknameForm = styled.div`
-  .inputLabel {
-    position: relative;
-    width: 100%;
-    font-size: 14px;
-
-    label {
-      position: absolute;
-      top: 8px;
-      left: 12px;
-      color: var(--grey700);
-    }
-
-    input {
-      width: 400px;
-      max-width: 100%;
-      height: 60px;
-      margin-bottom: 12px;
-      border-radius: 16px;
-      padding: 24px 12px 0 12px;
-      font-size: 16px;
-      font-weight: 400;
-      border: none;
-      border: 1px solid var(--grey400);
-      color: var(--grey900);
-      font-weight: 300;
-
-      ::placeholder {
-        color: var(--grey400);
-        font-weight: 300;
-      }
-    }
-}
-`
+`;
