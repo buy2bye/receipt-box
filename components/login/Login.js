ba50fe3f9243c81016d8eda5844c9673a12acc30
jungwoo <westhyena@gmail.com>
2022-02-12 15:21:32 +0900

    chore: remove unusing auth related codes

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,16 +1,13 @@
 import styled from '@emotion/styled';
-import useMe from 'hooks/useMe';
 import { useState } from 'react';
 import Tutorial from '/components/login/Tutorial';
 import { KAKAO_AUTH_URL } from 'helpers/oauth/kakao';
 
 const Login = () => {
-  const { mutate } = useMe();
   const [isEmailLoginFormShown, setIsEmailLoginFormShown] = useState(false);
 
   const handleKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL
-    // mutate(true, false);
   };
   const handleEmailLogin = () => {
     setIsEmailLoginFormShown(true);
