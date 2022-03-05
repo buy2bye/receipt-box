64f943abb54ed171c187bb236a942b5f5fb0ebac
kimnpop <kkrlcakes@gmail.com>
2022-03-05 18:24:02 +0900

    개인정보처리방침 적용

---
+++ pages/agreements/privacy-policy.js
@@ -0,0 +1,11 @@
+import 개인정보처리방침 from 'components/signup/agreements/개인정보처리방침';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <개인정보처리방침 />
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
