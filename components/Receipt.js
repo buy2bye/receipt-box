1e37abac3fb1f4f83164fdcaf248391528b15c7f
kimnpop <kkrlcakes@gmail.com>
2022-03-12 12:05:50 +0900

    영수증리스트 이미지 사이즈 조정

--- components/Receipt.js
+++ components/Receipt.js
@@ -55,8 +55,8 @@ const Container = styled.div`
     border: 1px solid var(--grey100);
 
     img {
-      width: 80%;
-      height: 80%;
+      width: 100%;
+      height: 100%;
       object-fit: cover;
       border-radius: 8px;
     }
