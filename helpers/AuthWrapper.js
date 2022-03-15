fc3172455cdc27bb4ab7b75311e91ee9960f416f
kimnpop <kkrlcakes@gmail.com>
2022-03-15 18:12:39 +0900

    쿼리 파라미터 물고 들어온 경우 로그인 페이지 redirect시 해당 파라미터 그대로 전달

--- helpers/AuthWrapper.js
+++ helpers/AuthWrapper.js
@@ -9,21 +9,21 @@ const WrapAuthPage = (WrapperComponent, isLoginPage, isSNSSignupPage) => {
 
   Wrapper.getInitialProps = async (ctx) => {
     const { bz_tracking_id } = ctx.query;
+
+    const queryParams = ctx.asPath.slice(ctx.pathname.length);
+
     // token 검증
     const accessToken = getCookie('accessToken', ctx);
     const refreshToken = getCookie('refreshToken', ctx);
     if (!isLoginPage && (!accessToken || !refreshToken)) {
-      if (bz_tracking_id)
-        redirect(`/login?bz_tracking_id=${bz_tracking_id}`, ctx);
-      else redirect('/login', ctx);
+      if (bz_tracking_id) redirect(`/login${queryParams}`, ctx);
+      else redirect(`/login${queryParams}`, ctx);
     } else if (isLoginPage && accessToken && refreshToken) {
       redirect('/', ctx);
     } else if (!isLoginPage) {
-      const res = await apiController({ ctx: ctx }).get(
-        '/api/user/info'
-      );
+      const res = await apiController({ ctx: ctx }).get('/api/user/info');
       if (res) {
-        const { data: userInfo } = res
+        const { data: userInfo } = res;
         if (!isSNSSignupPage && !userInfo.username && !userInfo.nickname) {
           // SNS 유저인데 닉네임 설정을 안했다
           redirect('/snssignup', ctx);
