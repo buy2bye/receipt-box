bfbc17aca2bf76c409b06b4c6a48d378f3e1bbfa
jin <slowbird.korea@gmail.com>
2022-06-08 20:19:55 +0900

    css수정3

--- components/receipt/collection/CollectionList.js
+++ components/receipt/collection/CollectionList.js
@@ -97,7 +97,7 @@ const CreateCollectionButton = styled.button`
   white-space: nowrap;
   padding: 4px 16px;
   height: 32px;
-  border: 1px solid var(--grey400);
+  border: 1px solid var(--grey100);
   border-radius: 12px;
   opacity: ${(props) => (props.isCollectionListOpen ? 1 : 0)};
   transition: 0.4s all;
