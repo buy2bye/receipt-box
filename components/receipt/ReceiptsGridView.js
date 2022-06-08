beb638ddd551f02249c4cc9cf23c5cb1f951ccb6
kimnpop <kkrlcakes@gmail.com>
2022-06-09 00:23:18 +0900

    배포 재시도

--- components/receipt/ReceiptsGridView.js
+++ components/receipt/ReceiptsGridView.js
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { useState } from 'react';
 import ReceiptGrid from './ReceiptGrid';
 
 const ReceiptsGridView = ({
